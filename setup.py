c05d0f70a056e4ef57699465ece5a58e9c3e2d08
Frenk Out <outfrenk@gmail.com>
2019-11-27 10:41:19 +0000

    updated setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+import os
+setup(name='dutchgic',
+      version='1.0',
+      description='GIC calculation in Dutch powergrid',
+      url='https://github.com/outfrenk/Dutch_GIC',
+      author='Frenk Out',
+      author_email='outfrenk@gmail.com',
+      license='MIT',
+      packages=find_packages(include=['Dutch_GIC'],exclude=['Tests']),
+      install_requires=['pandas','numpy','scipy','matplotlib'],
+      tests_require=['pytest'],
+      package_data={'dutchgic': ['network/*.csv']}
+      )
