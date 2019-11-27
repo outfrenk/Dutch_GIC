6895081064526899e48289ee796efe93b94b7ce2
Frenk Out <outfrenk@gmail.com>
2019-11-27 11:14:45 +0000

    changed setup

--- Dutch_GIC/Tests/test_code.py
+++ Dutch_GIC/Tests/test_code.py
@@ -22,12 +22,10 @@ def test_download(tmpdir):
     test.standard_download(['esk'])
     assert os.path.exists(f'{tmpdir}/12-03-1997/esk19970312dmin.min') is True
     assert os.path.exists(f'{tmpdir}/10-03-1997/esk19970310dmin.min') is True
-    os.system(f'rm -rf {tmpdir}')
     test=GIC(string,tmpdir,tmpdir,'31-12-2009')
     test.standard_download(['esk'])
     assert os.path.exists(f'{tmpdir}/31-12-2009/esk20091231dmin.min') is True
     assert os.path.exists(f'{tmpdir}/07-01-2010/esk20100107dmin.min') is True
-    os.system(f'rm -rf {tmpdir}')
     test=GIC(string,tmpdir,tmpdir,'1-1-2000')
     test.standard_download(['esk'])
     assert os.path.exists(f'{tmpdir}/01-01-2000/esk20000101dmin.min') is True
